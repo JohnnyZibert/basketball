85c8a48f98913d20761bacac2f545bc72ce47cca
zgirdan@mail.ru <you@example.com>
2022-11-15 16:54:22 +0300

    added loader to one page

--- src/Elements/Page/OnePage/OnePage.tsx
+++ src/Elements/Page/OnePage/OnePage.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
+import { selectOnePlayer } from '../../../Store/getOnePlayer/Selectors'
+import { selectOneTeam } from '../../../Store/getOneTeam/Selectors'
+import { Loader } from '../../../UI/Loader/Loader'
 import { getAge } from '../../../utils/utils'
 import styles from './OnePage.module.scss'
 
@@ -34,84 +38,107 @@ export const OnePage: React.FC<IProps> = ({
   weight,
   teamNameForPlayer,
 }) => {
+  const { status } = useSelector(selectOnePlayer)
+  const statusTeam = useSelector(selectOneTeam)
+
   return teamName ? (
-    <div className={styles.oneTeamsContainer}>
-      <div className={styles.wrapInfoTeam}>
-        <div className={styles.teamInfo}>
-          <div className={styles.logoTeams}>
-            <img src={imageUrl} className={styles.imgTeam} />
-          </div>
-          <div className={styles.descriptionTeam}>
-            <div className={styles.nameContainer}>{teamName}</div>
-            <div className={styles.infoBlock}>
-              <div className={styles.leftBlock}>
-                <div className={styles.foundation}>
-                  <div className={styles.teamData}>
-                    <div className={styles.label}>{'Year of foundation'}</div>
-                    <div className={styles.personInfo}>{foundationYear}</div>
-                  </div>
-                </div>
-                <div className={styles.teamData}>
-                  <div className={styles.label}>{'Conference'}</div>
-                  <div className={styles.personInfo}>{conference}</div>
-                </div>
+    <>
+      {statusTeam.status === 'loading' ? (
+        <Loader />
+      ) : (
+        <div className={styles.oneTeamsContainer}>
+          <div className={styles.wrapInfoTeam}>
+            <div className={styles.teamInfo}>
+              <div className={styles.logoTeams}>
+                <img src={imageUrl} className={styles.imgTeam} />
               </div>
-              <div className={styles.rightBlock}>
-                <div className={styles.teamData}>
-                  <div className={styles.label}>{'Division'}</div>
-                  <div className={styles.personInfo}>{division}</div>
+              <div className={styles.descriptionTeam}>
+                <div className={styles.nameContainer}>{teamName}</div>
+                <div className={styles.infoBlock}>
+                  <div className={styles.leftBlock}>
+                    <div className={styles.foundation}>
+                      <div className={styles.teamData}>
+                        <div className={styles.label}>
+                          {'Year of foundation'}
+                        </div>
+                        <div className={styles.personInfo}>
+                          {foundationYear}
+                        </div>
+                      </div>
+                    </div>
+                    <div className={styles.teamData}>
+                      <div className={styles.label}>{'Conference'}</div>
+                      <div className={styles.personInfo}>{conference}</div>
+                    </div>
+                  </div>
+                  <div className={styles.rightBlock}>
+                    <div className={styles.teamData}>
+                      <div className={styles.label}>{'Division'}</div>
+                      <div className={styles.personInfo}>{division}</div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   ) : (
-    <div className={styles.oneTeamsContainer}>
-      <div className={styles.wrapInfoTeam}>
-        <div className={styles.teamInfo}>
-          <div className={styles.logoPlayers}>
-            <img src={avatarUrl} className={styles.logoPlayers} />
-          </div>
-          <div className={styles.descriptionTeam}>
-            <div className={styles.nameContainer}>
-              <div className={styles.namePlayer}>
-                {playerName}
-                <span className={styles.numberPlayer}> #{number}</span>
+    <>
+      {status === 'loading' ? (
+        <Loader />
+      ) : (
+        <div className={styles.oneTeamsContainer}>
+          <div className={styles.wrapInfoTeam}>
+            <div className={styles.teamInfo}>
+              <div className={styles.logoPlayers}>
+                <img src={avatarUrl} className={styles.logoPlayers} />
               </div>
-            </div>
-            <div className={styles.infoBlock}>
-              <div className={styles.leftBlock}>
-                <div className={styles.foundation}>
-                  <div className={styles.teamData}>
-                    <div className={styles.label}>{'Position'}</div>
-                    <div className={styles.personInfo}>{position}</div>
+              <div className={styles.descriptionTeam}>
+                <div className={styles.nameContainer}>
+                  <div className={styles.namePlayer}>
+                    {playerName}
+                    <span className={styles.numberPlayer}> #{number}</span>
                   </div>
                 </div>
-                <div className={styles.teamData}>
-                  <div className={styles.label}>{'Height'}</div>
-                  <div className={styles.personInfo}>{height}</div>
-                </div>
-                <div className={styles.age}>
-                  <div className={styles.label}> {'Age'}</div>
-                  <div className={styles.personInfo}>{getAge(birthday)}</div>
-                </div>
-              </div>
-              <div className={styles.rightBlock}>
-                <div className={styles.teamData}>
-                  <div className={styles.label}>{'Team'}</div>
-                  <div className={styles.personInfo}>{teamNameForPlayer}</div>
-                </div>
-                <div className={styles.teamData}>
-                  <div className={styles.label}>{'Weight'}</div>
-                  <div className={styles.personInfo}>{weight}</div>
+                <div className={styles.infoBlock}>
+                  <div className={styles.leftBlock}>
+                    <div className={styles.foundation}>
+                      <div className={styles.teamData}>
+                        <div className={styles.label}>{'Position'}</div>
+                        <div className={styles.personInfo}>{position}</div>
+                      </div>
+                    </div>
+                    <div className={styles.teamData}>
+                      <div className={styles.label}>{'Height'}</div>
+                      <div className={styles.personInfo}>{height}</div>
+                    </div>
+                    <div className={styles.age}>
+                      <div className={styles.label}> {'Age'}</div>
+                      <div className={styles.personInfo}>
+                        {getAge(birthday)}
+                      </div>
+                    </div>
+                  </div>
+                  <div className={styles.rightBlock}>
+                    <div className={styles.teamData}>
+                      <div className={styles.label}>{'Team'}</div>
+                      <div className={styles.personInfo}>
+                        {teamNameForPlayer}
+                      </div>
+                    </div>
+                    <div className={styles.teamData}>
+                      <div className={styles.label}>{'Weight'}</div>
+                      <div className={styles.personInfo}>{weight}</div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
